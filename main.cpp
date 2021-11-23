461dd5d4046e04536e3bacf2574b27cbc292d50a
Logan Scholz <80925426+LoganScholz-PING@users.noreply.github.com>
2021-11-23 15:43:12 -0700

    Add json deserialization
    Added JSON deserialization example for WORKORDER API call (still TODO for Sales Order)

--- main.cpp
+++ main.cpp
@@ -8,20 +8,26 @@
 #include <WiFi.h> // important NOT to use the default Arduino library
 #include <HTTPClient.h>
 #include "WifiConfig.h" // for password and SSID
+#include "ArduinoJson.h"
 
 #define SIZE_OF_MAC_ADDRESS 6 // 6 bytes
 
 HTTPClient http;
 WiFiClient wificlient;
 
+//StaticJsonDocument<12288> so_json; // byte size calculated from https://arduinojson.org/v6/assistant/
+StaticJsonDocument<768> wo_json; // byte size calculated from https://arduinojson.org/v6/assistant/
+
+bool debug = false; // control print statements
+
 
 /*************************************************************
 * Connecting to PING REST API
 * Need the following headers:
     
-  -Organization:US
-  -Content-Type:application/json
-  -Ocp-Apim-Subscription-Key:
+  - Organization:US
+  - Content-Type:application/json
+  - Ocp-Apim-Subscription-Key:c480f6e38c64410199d9e003baf7ef24  
 
 * Need the following params:
 
@@ -31,12 +37,11 @@ void connectToInternetHTTP()
 {
   if (WiFi.status() == WL_CONNECTED)
   {
-    const char* conn = "Connectingto:";
-    Serial.println(conn);
+    Serial.print("Connecting to: ");
     
-    String workorder_num = "9267166"; // eventually this will be read out of an RFID tag(??)
+    char workorder_num[] = "9267166"; // eventually this will be read out of an RFID tag(??)
 
-    String FULL_API_PATH = String(PING_REST_API_HOST) + url + workorder_num;
+    String FULL_API_PATH = String(PING_REST_API_HOST) + wo_url + workorder_num;
 
     Serial.println(FULL_API_PATH);
 
@@ -47,28 +52,63 @@ void connectToInternetHTTP()
     http.addHeader("Content-Type", PING_CONTENT_TYPE);
     http.addHeader("Ocp-Apim-Subscription-Key", PING_OCP_AUTH);
 
-    int httpCode = http.GET();
+    int httpCode = http.GET(); // i'm pretty sure this line is blocking. There is an Async method I have not tried yet
+
+    // TODO:
+    // create a JSON object to hold the data returned by wificlient.readStringUntil('\r');
+    // then deserialize this object and print the following:
+    //    masterWorkOrderNumber
+    //    orderNumber
+    //    itemNumber
 
     if (httpCode > 0)
     {
       String payload = http.getString();
-      Serial.println("    * httpCode: " + String(httpCode));
-      Serial.println("    *  payload: " + payload);
+      Serial.println();
+      Serial.println("*** http.GET() DETAILS ***");
+      if(debug) Serial.println("  * httpCode: " + String(httpCode));
+      Serial.println("  *  payload: ");
+      Serial.println(payload);
+      Serial.println("****** ATTEMPT JSON PARSE BELOW ******");
+
+      DeserializationError DE = deserializeJson(wo_json, payload);
+
+      if (DE)
+      {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(DE.f_str());
+        return;
+      }
+
+      int wo_num = wo_json["workOrderNumber"];
+      int master_wo_num = wo_json["masterWorkOrderNumber"];
+      int order_num = wo_json["orderNumber"];
+      double item_num = wo_json["itemNumber"];
+      Serial.print("workOrderNumber: "); 
+      Serial.println(wo_num);
+      Serial.print("masterWorkOrderNumber: ");
+      Serial.println(master_wo_num);
+      Serial.print("orderNumber: ");
+      Serial.println(order_num);
+      Serial.print("itemNumber: ");
+      Serial.println(item_num, 2);
     }
     else
     {
+      // likely internal error to ESP32
       Serial.println("[ERROR] Error on HTTP request");
     }
 
     http.end();
-    }
-    else
-    {
-      Serial.println("[ERROR] Attempt GET but not connected to WiFi!");
-    }
+  }
+  else
+  {
+    Serial.println("[ERROR] Attempt GET but not connected to WiFi!");
+  }
 }
 
-
+// connectToInternetWiFi() just searches google.com for "arduino" and prints the returned results.
+// purely for testing and evaluation
 void connectToInternetWiFi()
 {
   const char* host = "google.com"; // for testing
@@ -127,10 +167,11 @@ void printScanNetworkInfo()
     return;
   }
 
-  Serial.print("WiFi Scan Complete. # Networks found: ");
-  Serial.println(n);
+  if(debug) Serial.print("WiFi Scan Complete. # Networks found: ");
+  if(debug) Serial.println(n);
 
-/* WIFI AUTH ENUMS
+  /*********************************
+  WIFI AUTH ENUMS
     WIFI_AUTH_OPEN = 0,     
     WIFI_AUTH_WEP = 1,            
     WIFI_AUTH_WPA_PSK = 2,         
@@ -138,32 +179,39 @@ void printScanNetworkInfo()
     WIFI_AUTH_WPA_WPA2_PSK = 4,     
     WIFI_AUTH_WPA2_ENTERPRISE = 5, 
     WIFI_AUTH_MAX = 6
-*/
+  *********************************/
 
 
-  for (int i = 0; i < n; ++i)
+  if(debug) 
   {
-    Serial.print(i + 1);
-    Serial.print(": ");
-    Serial.print(WiFi.SSID(i));
-    Serial.print(" (");
-    Serial.print(WiFi.RSSI(i)); // received signal strength indicator
-    Serial.print(")");
-    Serial.print( (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? "-OPEN" : "-SECURED" );
-    Serial.print(" [");
-    Serial.print(WiFi.encryptionType(i));
-    Serial.println("]");
-    delay(10);
+    for (int i = 0; i < n; ++i)
+    {
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i)); // received signal strength indicator
+      Serial.print(")");
+      Serial.print( (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? "-OPEN" : "-SECURED" );
+      Serial.print(" [");
+      Serial.print(WiFi.encryptionType(i));
+      Serial.println("]");
+      delay(10);
+    }
   }
 }
 
 void printMACAddress()
 {
   WiFi.disconnect();
-  Serial.print(F("Retrieving ESP32 MAC Address....\0"));
-  Serial.print(WiFi.macAddress());
-  Serial.println("");
-  Serial.flush();
+
+  if(debug) 
+  {
+    Serial.print(F("Retrieving ESP32 MAC Address... "));
+    Serial.print(WiFi.macAddress());
+    Serial.println("");
+    Serial.flush();
+  }
   
   //Serial.print("Easy MAC Address: ");
   //Serial.println(WiFi.macAddress()); // easy way to get MAC ID
@@ -190,8 +238,8 @@ void connectToWiFi()
   // strcpy(buff, test1);
   // strcat(buff, SSID);
   // Serial.println(buff);
-  Serial.print("Connecting-to: ");
-  Serial.println(SSID);
+  if(debug) Serial.print("Connecting to: ");
+  if(debug) Serial.println(SSID);
 
   // start WPA2 enterprise config magic
   WiFi.disconnect(true);
@@ -224,16 +272,17 @@ void connectToWiFi()
     delay(500);
 
     // more verbose print statements for the current status of the connection
+    
     switch( WiFi.status() )
     {
       case WL_NO_SHIELD:
-        Serial.println("WiFi.status() == WL_NO_SHIELD");
+        if(debug) Serial.println("WiFi.status() == WL_NO_SHIELD");
         break;
       case WL_IDLE_STATUS:
-        Serial.println("WiFi.status() == WL_IDLE_STATUS");
+        if(debug) Serial.println("WiFi.status() == WL_IDLE_STATUS");
         break;
       case WL_NO_SSID_AVAIL:
-        Serial.println("WiFi.status() == WL_NO_SSID_AVAIL");
+        if(debug) Serial.println("WiFi.status() == WL_NO_SSID_AVAIL");
         error_count += 1;
         if(error_count >= 10)
         {
@@ -242,19 +291,19 @@ void connectToWiFi()
         }
         break;
       case WL_SCAN_COMPLETED:
-        Serial.println("WiFi.status() == WL_SCAN_COMPLETED");
+        if(debug) Serial.println("WiFi.status() == WL_SCAN_COMPLETED");
         break;
       case WL_CONNECTED:
-        Serial.println("WiFi.status() == WL_CONNECTED");
+        if(debug) Serial.println("WiFi.status() == WL_CONNECTED");
         break;
       case WL_CONNECT_FAILED:
-        Serial.println("WiFi.status() == WL_CONNECT_FAILED");
+        if(debug) Serial.println("WiFi.status() == WL_CONNECT_FAILED");
         break;
       case WL_CONNECTION_LOST:
-        Serial.println("WiFi.status() == WL_CONNECTION_LOST");
+        if(debug) Serial.println("WiFi.status() == WL_CONNECTION_LOST");
         break;
       case WL_DISCONNECTED:
-        Serial.println("WiFi.status() == WL_DISCONNECTED");
+        if(debug) Serial.println("WiFi.status() == WL_DISCONNECTED");
         break;
       default:
         Serial.print("Unable to interpret WiFi.status() [");
@@ -265,9 +314,7 @@ void connectToWiFi()
   }
 
   Serial.println("");
-  Serial.println("WiFi connected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("Connected to " + String(SSID) + ". IP Address: " + String(WiFi.localIP()));
 }
 
 void setup() 
@@ -275,16 +322,16 @@ void setup()
   Serial.begin(115200);
   delay(10); // settle serial
 
-  Serial.println("***********PRINT MAC ADDRESS************");
+  if(debug) Serial.println("***********PRINT MAC ADDRESS************");
   printMACAddress();
   delay(500);
-  Serial.println("*********PRINT SCAN NETWORK INFO********");
+  if(debug) Serial.println("*********PRINT SCAN NETWORK INFO********");
   WiFi.disconnect(true);
   printScanNetworkInfo();
   delay(500);
-  Serial.println("*************CONNECT TO WIFI************");
+  if(debug) Serial.println("*************CONNECT TO WIFI************");
   connectToWiFi();
-  Serial.println("****************************************");
+  if(debug) Serial.println("****************************************");
 }
 
 
